75fdae3c7c3fb7033deb0bc96f2f7b7c67b00f69
Your Name <rashidaynur.ibram@yandex.com>
2019-11-23 19:32:41 +0000

    lastchaged

--- app.js
+++ app.js
@@ -359,7 +359,6 @@ app.post("/login", (req, res) => {
 });
 // *************************************
 
-app.listen(process.env.PORT || 3030, function () {
-  console.log("Server started on port 3030");
+app.listen(process.env.PORT || 3030, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-
