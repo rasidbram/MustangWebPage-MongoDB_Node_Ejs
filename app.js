0f9df48644e234d31fd4ad246b104f760ddd00d1
rasid bram <rashidaynur.ibram@yandex.com>
2020-10-24 01:23:20 +0100

    procfile edited

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
+// const ejs = require("ejs");
 const mongoose = require("mongoose");
-const session = require("express-session");
+// const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
