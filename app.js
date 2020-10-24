3d92478832e3030b18e66fab1801a3454f24eef9
rasid bram <rashidaynur.ibram@yandex.com>
2020-10-24 02:25:26 +0100

    mongo uri moved to env

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // set up mongodb*********************************************
-mongoose.connect("mongodb://localhost:27017/mustangDB", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
